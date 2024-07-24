c9816ac7259aba3209f9020d435e944c3c6d2d18
Abhishek <ab9861492097@gmail.com>
2024-07-24 17:09:41 +0930

    Resolved conflicts

---
+++ calculator.py
@@ -0,0 +1,11 @@
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Addition: ", add(1, 2))
+    
+    print("Subtraction: ", subtract(2, 1))
